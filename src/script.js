1f2c362bc5ead07fc92a6430186253352a9513a4
Shantanu Rathod <contactshantanurathod@gmail.com>
2022-11-04 21:27:36 +0530

    adding all files

---
+++ src/script.js
@@ -0,0 +1,135 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug
+const gui = new dat.GUI()
+gui.close()
+gui.hide()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Objects
+const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const geometry = new THREE.BufferGeometry()
+
+// const count = 50
+// const vertexArray = new Float32Array(count*3*3)
+
+// for(let i = 0; i < count*3*3; i++)
+// {
+//     vertexArray[i] = (Math.random() - 0.5)*5
+// }
+
+// geometry.setAttribute('position', new THREE.BufferAttribute(vertexArray, 3))
+
+// Materials
+const material = new THREE.MeshNormalMaterial()
+material.color = new THREE.Color(0xff0000)
+// material.wireframe = true
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+// camera.lookAt(sphere.position)
+sphere.position.set(0, 0, 0)
+
+
+/**
+ * Full screen
+ */
+window.addEventListener('dblclick', () => {
+    if (!document.fullscreenElement) {
+        canvas.requestFullscreen()
+    }
+    else {
+        document.exitFullscreen()
+    }
+})
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // camera.rotation.y = Math.sin(elapsedTime)
+    sphere.rotation.y = elapsedTime
+    sphere.position.x = Math.sin(elapsedTime)
+
+    // Update Orbital Controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
